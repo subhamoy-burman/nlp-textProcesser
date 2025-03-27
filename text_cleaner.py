d1a860d7e9882d3b1290d07caf318b09b902a548
subhamoy-burman <subhamoy.2.burman@gmail.com>
2025-03-27 20:53:55 +0530

    Lemmatization and stemming

--- text_cleaner.py
+++ text_cleaner.py
@@ -10,7 +10,23 @@ nltk.download('wordnet') # For lemmatization later
 nltk.download('averaged_perceptron_tagger')  # For POS tagging
 nltk.download('maxent_ne_chunker')  # For NER
 nltk.download('words') #Also for NER and other purposes
+nltk.download('averaged_perceptron_tagger')
+nltk.download('averaged_perceptron_tagger_eng')
+from nltk.corpus import wordnet
 
+def get_wordnet_pos(nltk_tag):
+    """Map NLTK POS tags to WordNet POS tags."""
+    if nltk_tag.startswith('J'):
+        return wordnet.ADJ
+    elif nltk_tag.startswith('V'):
+        return wordnet.VERB
+    elif nltk_tag.startswith('N'):
+        return wordnet.NOUN
+    elif nltk_tag.startswith('R'):
+        return wordnet.ADV
+    else:
+        # Default to noun if mapping is unclear
+        return wordnet.NOUN
 
 def preprocess_text(filePath):
     with open(filePath, 'r', encoding='utf-8') as file:
@@ -19,14 +35,14 @@ def preprocess_text(filePath):
         sentences = nltk.sent_tokenize(text)
         print("\n Printing sentences after tokenization: \n \n",sentences)
 
-        words = []
+        wordsInitial = []
         for sent in sentences:
             words.extend(nltk.word_tokenize(sent))
-        words = [w.lower() for w in words]
-        print("\n Printing words after tokenization: \n \n",words)
+        wordsInitial = [w.lower() for w in words]
+        print("\n Printing words after tokenization: \n \n",wordsInitial)
 
         stopwords = nltk.corpus.stopwords.words('english')
-        words = [w for w in words if w not in stopwords]
+        words = [w for w in wordsInitial if w not in stopwords]
         print("\n Printing words after removing stopwords: \n \n",words)
 
         words = [w for w in words if w not in string.punctuation]
@@ -34,5 +50,20 @@ def preprocess_text(filePath):
 
         words = [w for w in words if w.isalpha()]
         print("\n Printing words after removing non-alphabetic characters: \n \n",words)
+
+        stemmer = PorterStemmer()
+        stemmed_words = [stemmer.stem(w) for w in words]
+        print("\n Printing words after stemming: \n \n",stemmed_words)
+
+        lemmatizer = WordNetLemmatizer()
+        lemmatized_words = [lemmatizer.lemmatize(w) for w in words]
+        print("\n Printing words after lemmatization: \n \n",lemmatized_words)
+
+        pos_tags = nltk.pos_tag(words)
+        print("\n--- POS Tags ---")
+        print(pos_tags)
+
+        lemmatized_words_pos = [lemmatizer.lemmatize(word, get_wordnet_pos(tag)) for word, tag in pos_tags]
+        print("\n Printing words after POS-aware lemmatization: \n \n", lemmatized_words_pos)
 
 preprocess_text('simple.txt')
